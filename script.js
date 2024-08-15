29728e9cf18c68cf45284ae4350922a3eb8a5b23
duzinhoy <127217308+duzinhoy@users.noreply.github.com>
2024-08-15 10:49:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa 2",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa 3",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa 4",
+            "Alternativa 2"
+        ]
+    }
+];
